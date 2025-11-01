e23442d57f9cfdba856127830a369f0aada0ab92
kizuto2006 <vhung7986@gmail.com>
2025-11-01 23:34:41 +0700

    buoi2

---
+++ BTVN/Buổi 2/B2/B2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.Write("Nhập chuỗi kí tự: ");
+string a = Console.ReadLine();
+Console.Write("Chuỗi kí tự đã đảo ngược: ");
+for(int i = a.Length - 1; i >= 0; i--)
+{
+    Console.Write(a[i]);
+}
+Console.WriteLine();
---
+++ BTVN/Buổi 2/B3/B3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    public static 
+    public static void Main(string[] args)
+    {
+        Dictionary<string,Product> products = new Dictionary<string,Product>();
+
+
+    }
+
+    struct Product
+    {
+        string Name;
+        double Price;
+        int Quantity;
+        string Category;
+    }
+}
